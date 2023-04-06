bf0ae1438a91adb2572e6695c370569132976ccf
psh2428 <simon2428@naver.com>
2023-04-06 15:08:02 +0900

    Create 14681.cpp

---
+++ 14681.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+void main() {
+	int x;
+	int y;
+	scanf_s("%d\n", &x);
+	scanf_s("%d", &y);
+
+	if (x > 0 && y > 0) {
+		printf("1");
+	}
+	else if (x > 0 && y < 0) {
+		printf("2");
+	}
+	else if (x < 0 && y < 0) {
+		printf("3");
+	}
+	else if (x < 0 && y>0) {
+		printf("4");
+	}
+}
