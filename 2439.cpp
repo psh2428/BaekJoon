2cdd3707ffb5392957b24fc2329f02ddc41a6335
psh2428 <simon2428@naver.com>
2023-04-06 15:09:25 +0900

    Create 2439.cpp

---
+++ 2439.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int num, i, j;
+	scanf("%d", &num);
+	for (i = 1; i<=num; i++) {
+		for (j = 1; j <= (num - i); j++) {
+			printf(" ");
+		}
+		for (j = 1; j <= i; j++){ 
+			printf("*");
+		}
+			
+		printf("\n");
+		
+	
+	}
+	
+	
+}
