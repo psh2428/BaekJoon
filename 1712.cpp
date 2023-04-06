3fe4f86bb8134589d3cd3bf8c5e20422a1c381b4
psh2428 <simon2428@naver.com>
2023-04-06 15:09:13 +0900

    Create 1712.cpp

---
+++ 1712.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+void main() {
+	int A, B, C;
+	scanf("%d %d %d", &A, &B, &C);
+	if (A, B, C > 210000000) {
+		printf("오류입니다.");
+	}
+	
+	if (B >= C) {
+		printf("-1");
+	}
+	else {
+		printf("%d", A / (C - B) + 1);
+	}
+	
+	
+	
+}
