cc1f9aff0afa707780a609895d788a2b6211f7fe
psh2428 <simon2428@naver.com>
2023-04-06 15:09:36 +0900

    Create 2525 retry.cpp

---
+++ 2525 retry.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+void main() {
+	int hour, min, c;
+	scanf("%d %d", &hour, &min);
+	scanf("%d", &c);
+	int min2 = min + c;
+	int min3 = min2 / 60;
+	int min4 = min2 % 60;
+	if (min2 > 60) {
+		hour += min3;
+		if (hour >= 24) {
+			hour = hour - 24;
+		}
+		printf("%d %d", hour, min4);
+
+	}
+	else if (min2 < 60) {
+		if (hour >= 24) {
+			hour - 24;
+		}
+		printf("%d %d", hour, min2);
+	}
+
+	
+}
