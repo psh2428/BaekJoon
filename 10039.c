855b63c74e4a763f187ee56b53a2d786e0dc14df
psh2428 <simon2428@naver.com>
2023-04-06 14:59:15 +0900

    Create 10039.c

---
+++ 10039.c
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int total = 0, i, tmp;
+	for (i = 0; i < 5; i++) {
+		scanf("%d", &tmp);
+		if (tmp < 40) {
+			tmp = 40;
+		}
+		total += tmp;
+	}
+	printf("%d", total / 5);
+	return 0;
+}
