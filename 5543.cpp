55a675b9e43806bf91efb5a0042f296537b2832a
psh2428 <simon2428@naver.com>
2023-04-06 15:09:40 +0900

    Create 5543.cpp

---
+++ 5543.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void main() {
+	int n, m, min = 2000, min2 = 2000;
+	int i, j;
+	int sum;
+	for (i = 0; i < 3; i++) {
+		scanf("%d", &n);
+		if (min > n)
+		min = n;
+	}
+
+	for (j = 0; j < 2; j++) {
+		scanf("%d", &m);
+		if (min2 > m)
+			min2 = m;
+	}
+	sum = min + min2 - 50;
+	printf("%d", sum);
+	
+	
+
+}
