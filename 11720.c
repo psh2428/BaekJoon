c5eb2fa682595ee518e69fc5e6cd76eded8801da
psh2428 <simon2428@naver.com>
2023-04-06 15:07:59 +0900

    Create 11720.c

---
+++ 11720.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void main() {
+	int sum = 0;
+	int n;
+	char arr[100];
+	scanf("%d", &n);
+	scanf("%s", arr);
+	for (int i = 0; i < n; i++) {
+		sum += arr[i]-'0';
+	}
+	printf("%d", sum);
+	
+
+
+
+}
