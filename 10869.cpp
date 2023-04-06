a27c28d03b223c9e240928c049e94dc8781cb024
psh2428 <simon2428@naver.com>
2023-04-06 15:07:56 +0900

    Create 10869.cpp

---
+++ 10869.cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void main() {
+	int a;
+	int b;
+	scanf("%d", &a);
+	if (a < 1) {
+		printf("값을 잘못 입력하였습니다.");
+		return;
+	}
+	scanf("%d", &b);
+	if (b > 10000) {
+		printf("값을 잘못 입력하였습니다.");
+		return;
+	}
+	printf("%d\n", a + b);
+	printf("%d\n", a - b);
+	printf("%d\n", a * b);
+	printf("%d\n", a / b);
+	printf("%d\n", a % b);
+
+}
