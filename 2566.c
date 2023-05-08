7610331e67a1bc4cf021d2eb124e69d3a259971d
psh2428 <simon2428@naver.com>
2023-05-08 17:09:48 +0900

    Update 2566.c

--- 2566.c
+++ 2566.c
@@ -7,7 +7,7 @@ int main() {
 	int arr[9][9] = { 0, };
 	for (i = 0; i < 9; i++) {
 		for (j = 0; j < 9; j++) {
-			scanf("%d ",&arr[i][j]);
+			scanf("%d",&arr[i][j]);
 			if (max <= arr[i][j]) {
 				max = arr[i][j];
 				x = i;
